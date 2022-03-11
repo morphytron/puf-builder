b728188242e65c20794640c43b94e3a7e07af2fd
Apatiga, Daniel <77174601+morphytron@users.noreply.github.com>
2022-03-10 22:59:59 -0500

    Delete target/release directory

--- target/release/pufb.d
+++
@@ -1,1 +0,0 @@
-/Users/solumyr/github/puf-builder/target/release/pufb: /Users/solumyr/github/puf-builder/src/builder.rs /Users/solumyr/github/puf-builder/src/io.rs /Users/solumyr/github/puf-builder/src/main.rs
